96246bc1a0e7edfa7180a675899626d187620261
quoeamaster <quoeamaster@gmail.com>
2018-06-04 18:56:54 +0800

    2018-06-04a

--- QPong/src/components/testing/Testing.vue
+++ QPong/src/components/testing/Testing.vue
@@ -17,6 +17,18 @@
           invoke <span class="powered success-color-dark white-font">QPongServer</span> on a testing api
         </div>
       </div>
+      <!-- testing on upload -->
+      <!-- testing on the go server -->
+      <div class="row">
+          <div class="col-md-1 col-sm-1 col-1 container-test-item">3.</div>
+          <div class="col-md-11 col-sm-11 col-11 container-test-bottom">
+            <form action="javascript:void(0)" method="post" enctype="multipart/form-data">
+              <input class="form-control form-control-file" type="file" name="pfile" id="pfile" />
+              <button class="btn btn-default btn-sm" @click="uploadTest_1()">go</button>
+              perform a file upload operation to <span class="powered success-color-dark white-font">QPongServer</span>
+            </form>
+          </div>
+      </div>
     </div>
     <!-- result panels -->
     <div>
@@ -83,6 +95,48 @@ export default {
         },
         dataType: null
       }) // end -- jquery.get()
+    },
+    /*
+     *  method to test file upload
+     */
+    uploadTest_1: function () {
+      let fileObj = window.jquery('#pfile')
+      if (fileObj.length > 0) {
+        let formData = new FormData()
+
+        fileObj = fileObj[0]
+        for (let i = 0; i < fileObj.length; i++) {
+          let file = fileObj[i]
+          // Check the file type.
+          if (!file.type.match('image.*')) {
+            continue
+          }
+          // Add the file to the request.
+          formData.append('pfile[]', file, file.name)
+        } // end -- for (files inside the fileObj)
+
+        // ajax call
+        window.jquery.ajax({
+          url: 'http://localhost:8081/testing/101',
+          type: 'POST',
+          headers: {
+            'X-My-Header': 'something'
+          },
+          data: formData,
+          cache: false,
+          dataType: 'json',
+          processData: false, // NEVER process the file(s)
+          contentType: false, // NEVER set contentType
+          success: function (data, textStatus, jqXHR) {
+            console.log('*success => ' + textStatus)
+            console.log(data)
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log('** error => ' + textStatus)
+            console.log(errorThrown)
+          }
+        })
+      } // end -- if (fileObj is valid)
     }
   }
 }
